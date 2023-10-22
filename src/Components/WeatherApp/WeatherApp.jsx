9cc417ccb219cf65cddf091848c8a15a81e165fc
ADITHS7 <adithsn777@gmail.com>
2023-10-22 13:16:02 +0530

    basic design and api integration completed

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -9,12 +9,54 @@ import snow_icon from '../Assets/snow.png';
 import wind_icon from '../Assets/wind.png';
 import humidity_icon  from '../Assets/humidity.png';
 const WeatherApp = () => {
+  let api_key = "d5c1af5af0e82e687002c816851825e4";
+  const search = async()=>{
+   const element=document.getElementsByClassName("cityInput");
+   if(element[0].value==="")
+   {
+    return 0
+   }
+   let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+   let response = await fetch(url);
+   let data = await response.json();
+   const humidity = document.getElementsByClassName("humidity-percent");
+   const wind = document.getElementsByClassName("wind-rate");
+   const temperature = document.getElementsByClassName("weather-temp");
+   const location = document.getElementsByClassName("weather-location");
+
+   humidity[0].innerHTML = data.main.humidity +" %";
+   wind[0].innerHTML=data.wind.speed +" km/hr";
+   temperature[0].innerHTML=data.main.temp +" °c";
+   location[0].innerHTML=data.name;
+   
+  }
   return (
     <div className='container'>
       <div className="top-bar">
         <input type='text' className='cityInput' placeholder='Search'/>
-        <div className="search-icon">
-          <img src={search_icon} alt=''/>
+        <div className="search-icon" onClick={()=>{search()}}>
+          <img src={search_icon} alt=''/>        
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={cloud_icon} alt=''/>
+      </div>
+      <div className="weather-temp">24 °c</div>
+      <div className="weather-location">London</div>
+      <div className="data-containers">
+        <div className="element">
+          <img src={humidity_icon} alt=''/>
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={wind_icon} alt=''/>
+          <div className="data">
+            <div className="wind-rate">18km/h</div>
+            <div className="text">Wind speed</div>
+          </div>
         </div>
       </div>
     </div>
